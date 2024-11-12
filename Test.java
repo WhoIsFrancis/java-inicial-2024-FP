c8892677dbb188878010f3f70905a6095140dcea
lean <liferreira90@gmail.com>
2024-11-12 13:48:05 -0300

    First commit

---
+++ Test.java
@@ -0,0 +1,47 @@
+import java.sql.SQLOutput;
+
+public class Test {
+    public static void main(String[] args) {
+        // tipos primitivos y su respectivo wrapper
+
+        byte varByte;
+        Byte aByte;
+
+        short varShort;
+        Short aShort;
+
+        int varInt;
+        Integer integer;
+
+        long varLong;
+        Long aLong;
+
+        float varFloat;
+        Float aFloat;
+
+        double varDouble;
+        Double aDouble;
+
+        char varChar;
+        Character aChar;
+
+        boolean varBoolean;
+        Boolean aBoolean;
+
+        // ejemplo modelado de atributos usando primitivos - Smartphone
+
+        short temperaturaMinimaDeFuncionamientoEnGradosCelsius = -12;
+        int ciclosDeCarga = 1685;
+        long totalPixelesPantalla = 165322985L;
+        float ciclosDelCpuPorSegundo = 12.456F;
+        double velocidadDeSwap = 546.542;
+        char version = 'A';
+        boolean es4g = false;
+
+        // Fabricante? Marca? Info
+
+        String fabricante = "XIAOMI";
+
+        System.out.println("abc");
+    }
+}
