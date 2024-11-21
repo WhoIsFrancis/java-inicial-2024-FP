a30fbe9939e72ae9a415e42272962b97d3eb5ce1
leanferreira17cba <liferreira90@gmail.com>
2024-11-21 08:47:11 -0300

    Se agrega tp restaurante

---
+++ src/main/java/restaurante/Pedido.java
@@ -0,0 +1,23 @@
+package restaurante;
+
+public class Pedido implements Comandable {
+    @Override
+    public void agregarComanda(Comanda comanda) {
+
+    }
+
+    @Override
+    public int recuperarCantidadDeComandasTotales() {
+        return 0;
+    }
+
+    @Override
+    public Comanda recuperarUltimaComandaAgregada() {
+        return null;
+    }
+
+    @Override
+    public void eliminarUltimaComanda() {
+
+    }
+}
